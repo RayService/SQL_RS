67002d0bd05f942fee208ed0dd6399d5f59f5ae9
RayService <michal.zufan@rayservice.com>
2025-06-25 22:02:41 +0200

    Create ENG_zadani_prace_Husek.sql

---
+++ ENG_zadani_prace_Husek.sql
@@ -0,0 +1,11 @@
+--kooperaèní dílce: øada VP=803
+--kusovníky: vyøadit 93x a 888
+--operace = ze seznamu postupu, najít první kooperaci (dle èísla operace)
+
+
+
+--kooperaèní dílce, které obsahují kooperaèní operaci s názvem "SurTec 650". Vložt jednicovou operaci s èíslem +1, a dle operace ze zlatého 574/03150 operaci 0481/Pøíjem z kooperace - SurTec 650
+--totéž pro operace TP - Lakování, TP - Sítotisk, TP - Obrábìní, TP - Výplò gravíru, TP - Lisování - analogické jednicové operace
+--kromì dílcù ve zmìnì ID=99076 a kromì SK=681
+
+
