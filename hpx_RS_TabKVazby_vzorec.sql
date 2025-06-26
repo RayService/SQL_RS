c2af7816fda1331c269014a3b41e2fdf62590230
RayService <michal.zufan@rayservice.com>
2025-06-26 10:06:38 +0200

    Create hpx_RS_TabKVazby_vzorec.sql

---
+++ hpx_RS_TabKVazby_vzorec.sql
@@ -0,0 +1,17 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_RS_TabKVazby_vzorec]    Script Date: 26.06.2025 10:06:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[hpx_RS_TabKVazby_vzorec]
+  @PrvniPruchod BIT
+AS
+
+SELECT 2 AS IDVzorceSpotMat
+GO
+
