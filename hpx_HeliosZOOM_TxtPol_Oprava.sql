4f3b712fac9afb373d02700656eaf0356a62e113
RayService <michal.zufan@rayservice.com>
2025-06-26 11:25:04 +0200

    Create hpx_HeliosZOOM_TxtPol_Oprava.sql

---
+++ hpx_HeliosZOOM_TxtPol_Oprava.sql
@@ -0,0 +1,34 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_HeliosZOOM_TxtPol_Oprava]    Script Date: 26.06.2025 11:25:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[hpx_HeliosZOOM_TxtPol_Oprava]
+@_EXT_B2A_NutnostInvestice NVARCHAR(10),
+@_EXT_B2A_ZpusobRealizace NVARCHAR(15),
+--@IDZakazka INT,
+--@Poznamka NTEXT,
+@ID INT
+AS
+
+BEGIN
+UPDATE  TabOZTxtPol_EXT SET
+_EXT_B2A_NutnostInvestice = @_EXT_B2A_NutnostInvestice,
+_EXT_B2A_ZpusobRealizace= @_EXT_B2A_ZpusobRealizace
+FROM TabOZTxtPol_EXT
+WHERE ID = @ID
+/*
+UPDATE txt SET
+txt.IDZakazka = @IDZakazka
+--,txt.Poznamka = @Poznamka
+FROM TabOZTxtPol txt
+WHERE txt.ID = @ID
+*/
+END
+GO
+
