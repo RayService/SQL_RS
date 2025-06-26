77378475cecfe2eb92383eb613b8f46073de52fe
RayService <michal.zufan@rayservice.com>
2025-06-26 09:23:11 +0200

    Create hpx_RSPersMzdy_ZamMzdyZapis.sql

---
+++ hpx_RSPersMzdy_ZamMzdyZapis.sql
@@ -0,0 +1,25 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_RSPersMzdy_ZamMzdyZapis]    Script Date: 26.06.2025 9:23:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[hpx_RSPersMzdy_ZamMzdyZapis]
+	@IDZam INT
+AS
+SET NOCOUNT ON;
+-- =============================================
+-- Author:		JDO
+-- Description: Stanoveni mzdy - zapis vypoctu do mzdovych udaju
+-- =============================================
+
+RAISERROR (N'Tato funkcionalita bude zprovozněna po odsouhlasení správnosti výsledků výpočtu!',16,1);
+RETURN;
+GO
+
