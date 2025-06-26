8872e38242b6448f0ff24576a7849ef50b5760cf
RayService <michal.zufan@rayservice.com>
2025-06-26 15:25:02 +0200

    Create hpx_SHTools_Viewer.sql

---
+++ hpx_SHTools_Viewer.sql
@@ -0,0 +1,19 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_SHTools_Viewer]    Script Date: 26.06.2025 15:24:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[hpx_SHTools_Viewer]
+@P1 nvarchar(MAX)
+,@P2 nvarchar(MAX)
+AS
+BEGIN
+SELECT 1 /* No action */
+END;
+GO
+
