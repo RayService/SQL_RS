42568b540a66c6800212b0540f8322446314e7a8
RayService <michal.zufan@rayservice.com>
2025-06-26 16:03:43 +0200

    Create hpx_RS_KopieUmisteniJinySklad.sql

---
+++ hpx_RS_KopieUmisteniJinySklad.sql
@@ -0,0 +1,56 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_RS_KopieUmisteniJinySklad]    Script Date: 26.06.2025 16:03:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[hpx_RS_KopieUmisteniJinySklad] @IDSklad NVARCHAR(30), @ID INT
+AS
+
+SET NOCOUNT ON;
+--DECLARE @IDSklad NVARCHAR(30)
+--DECLARE @ID INT
+--SET @IDSklad=N'100'
+--SET @ID=197552
+BEGIN
+--ostré deklarace
+DECLARE @Kod NVARCHAR(15);
+DECLARE @IDNew INT;
+SET @Kod=(SELECT Kod FROM TabUmisteni WHERE ID=@ID);
+
+--SELECT tu.Id,tu.IdSklad,tu.Kod,tu.Nazev,tue._VyjmoutZMapySkladu,tue._PovolitZaporneMnozNaUmisteni,tue._IDPolice,tue._EXT_B2ADIMARS_UseForQaReportInventoryEntryPrint,tue._EXT_RS_generate_stock_taking_subordinate,tue._EXT_RS_PhysicalPlace,tue._EXT_RS_IDLocationPrint,tue._EXT_RS_IDLocationPrint,tue._EXT_RS_VypnoutFIFOFEFO,tue._EXT_RS_VypnoutVzorec
+--FROM TabUmisteni tu
+--LEFT OUTER JOIN TabUmisteni_EXT tue ON tue.ID=tu.ID
+--WHERE tu.ID=@ID
+
+IF EXISTS (SELECT tu.Id FROM TabUmisteni tu WHERE tu.Kod=@Kod AND tu.IdSklad=@IDSklad)
+BEGIN
+RAISERROR('Na cílovém skladu již umístění se stejným kódem existuje.',16,1)
+RETURN
+END;
+
+INSERT INTO TabUmisteni (IdSklad,Kod,Nazev)
+SELECT @IDSklad,tu.Kod,tu.Nazev
+FROM TabUmisteni tu
+LEFT OUTER JOIN TabUmisteni_EXT tue ON tue.ID=tu.ID
+WHERE tu.ID=@ID
+SET @IDNew=(SELECT SCOPE_IDENTITY());
+
+INSERT INTO TabUmisteni_EXT (ID,_VyjmoutZMapySkladu,_PovolitZaporneMnozNaUmisteni,_IDPolice,_EXT_B2ADIMARS_UseForQaReportInventoryEntryPrint,_EXT_RS_generate_stock_taking_subordinate,_EXT_RS_PhysicalPlace,_EXT_RS_IDLocationPrint,_EXT_RS_VypnoutFIFOFEFO,_EXT_RS_VypnoutVzorec)
+SELECT @IDNew,tue._VyjmoutZMapySkladu,tue._PovolitZaporneMnozNaUmisteni,tue._IDPolice,tue._EXT_B2ADIMARS_UseForQaReportInventoryEntryPrint,tue._EXT_RS_generate_stock_taking_subordinate,tue._EXT_RS_PhysicalPlace,tue._EXT_RS_IDLocationPrint,tue._EXT_RS_VypnoutFIFOFEFO,tue._EXT_RS_VypnoutVzorec
+FROM TabUmisteni tu
+LEFT OUTER JOIN TabUmisteni_EXT tue ON tue.ID=tu.ID
+WHERE tu.ID=@ID;
+
+--SELECT tu.Id,tu.IdSklad,tu.Kod,tu.Nazev,tue._VyjmoutZMapySkladu,tue._PovolitZaporneMnozNaUmisteni,tue._IDPolice,tue._EXT_B2ADIMARS_UseForQaReportInventoryEntryPrint,tue._EXT_RS_generate_stock_taking_subordinate,tue._EXT_RS_PhysicalPlace,tue._EXT_RS_IDLocationPrint,tue._EXT_RS_IDLocationPrint,tue._EXT_RS_VypnoutFIFOFEFO,tue._EXT_RS_VypnoutVzorec
+--FROM TabUmisteni tu
+--LEFT OUTER JOIN TabUmisteni_EXT tue ON tue.ID=tu.ID
+--WHERE tu.Kod=@Kod AND tu.IdSklad=@IDSklad;
+
+END;
+GO
+
