56821fd7c3f507dd88d47a9fe8895d70a8e79b1d
RayService <michal.zufan@rayservice.com>
2025-06-26 09:36:11 +0200

    Create hpx_HeliosZOOM_KontJednani_Novy.sql

---
+++ hpx_HeliosZOOM_KontJednani_Novy.sql
@@ -0,0 +1,41 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_HeliosZOOM_KontJednani_Novy]    Script Date: 26.06.2025 9:36:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[hpx_HeliosZOOM_KontJednani_Novy] @Kategorie NVARCHAR(3),@Predmet NVARCHAR(255),@DatumJednaniOd DATETIME,@DatumJednaniDo DATETIME,@Typ NVARCHAR(3),@Stav NVARCHAR(3),@Popis NTEXT,@CisloOrg INT
+AS
+DECLARE @PoradoveCislo INT,@SkladZbozi NVARCHAR(30)
+
+DECLARE @Hlaska NVARCHAR(255)
+
+IF ISNULL(@Predmet ,N'') = N''
+ BEGIN
+  SET @Hlaska = dbo.hfx_TranslateString(N'8F76F6A3-F4FD-4D0B-BD82-C59AD4ABDC52')
+  IF @Hlaska IS NOT NULL
+   BEGIN
+    RAISERROR(@Hlaska, 16,1)
+	RETURN
+   END
+ END
+
+
+IF EXISTS(SELECT ID FROM TabKontaktJednani WHERE Kategorie = @Kategorie)
+ SELECT @PoradoveCislo = MAX(PoradoveCislo)+1  FROM TabKontaktJednani WHERE Kategorie = @Kategorie
+ELSE
+ SET @PoradoveCislo = 1
+
+SELECT @SkladZbozi = SkladZbozi FROM TabKategKontJed WHERE Cislo = @Kategorie
+
+
+INSERT INTO TabKontaktJednani
+  (PoradoveCislo, Kategorie, Predmet, DatumJednaniOd, DatumJednaniDo, Popis, Typ, Stav, DruhVystupu, Utvar, MistoKonani, CisloOrg, BlokovaniEditoru)
+VALUES
+  (@PoradoveCislo, @Kategorie, @Predmet, @DatumJednaniOd, @DatumJednaniDo, @Popis, @Typ, @Stav, N'', @SkladZbozi, N'', @CisloOrg, null)
+GO
+
