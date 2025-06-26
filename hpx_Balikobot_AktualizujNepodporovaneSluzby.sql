eba924af58e14c8440766b9b05cd5dae63066e8f
RayService <michal.zufan@rayservice.com>
2025-06-26 14:27:26 +0200

    Create hpx_Balikobot_AktualizujNepodporovaneSluzby.sql

---
+++ hpx_Balikobot_AktualizujNepodporovaneSluzby.sql
@@ -0,0 +1,15 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_Balikobot_AktualizujNepodporovaneSluzby]    Script Date: 26.06.2025 14:27:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*rpBalikobot.Export*/CREATE PROC [dbo].[hpx_Balikobot_AktualizujNepodporovaneSluzby]
+AS
+SELECT 1
+GO
+
