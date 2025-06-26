c6bc8cd4f708f04929731e987e2eae75f006c954
RayService <michal.zufan@rayservice.com>
2025-06-26 12:10:15 +0200

    Create hpx_RS_zmena_segmentu_zak.sql

---
+++ hpx_RS_zmena_segmentu_zak.sql
@@ -0,0 +1,33 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_RS_zmena_segmentu_zak]    Script Date: 26.06.2025 12:10:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[hpx_RS_zmena_segmentu_zak]
+@_EXT_RS_segmentace NVARCHAR(100),
+@kontrola bit,
+@ID INT
+AS
+IF @kontrola = 1
+BEGIN
+BEGIN TRANSACTION;
+UPDATE dbo.TabCisOrg_EXT WITH (UPDLOCK, SERIALIZABLE) SET _EXT_RS_segmentace=@_EXT_RS_segmentace WHERE ID = @ID;
+IF @@ROWCOUNT = 0
+BEGIN
+  INSERT dbo.TabCisOrg_EXT (ID,_EXT_RS_segmentace)
+  VALUES (@ID,@_EXT_RS_segmentace);
+END
+COMMIT TRANSACTION;
+END;
+ELSE
+BEGIN
+RAISERROR ('Nic neproběhne, není zatrženo Přepsat.',16,1);
+RETURN
+END;
+GO
+
