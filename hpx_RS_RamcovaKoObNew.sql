bddbddf4669f956aa24f1e6559d0bd188b1daf24
RayService <michal.zufan@rayservice.com>
2025-06-30 08:08:13 +0200

    Create hpx_RS_RamcovaKoObNew.sql

---
+++ hpx_RS_RamcovaKoObNew.sql
@@ -0,0 +1,19 @@
+USE [RayService]
+GO
+
+/****** Object:  StoredProcedure [dbo].[hpx_RS_RamcovaKoObNew]    Script Date: 30.06.2025 8:08:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[hpx_RS_RamcovaKoObNew]
+  @PrvniPruchod BIT
+AS
+
+
+-------------------------------------------------------------------
+GO
+
