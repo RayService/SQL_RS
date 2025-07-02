9f2d3732d501ac1e456005311fa4c5832e843c24
RayService <michal.zufan@rayservice.com>
2025-07-02 09:08:08 +0200

    Create Tabx_RS_IDKmenVOB.sql

---
+++ Tabx_RS_IDKmenVOB.sql
@@ -0,0 +1,16 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_IDKmenVOB]    Script Date: 02.07.2025 9:08:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_IDKmenVOB](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDKmen] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
