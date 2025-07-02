75449c06a3f4b9e314d96b24ada4fa86cdc60775
RayService <michal.zufan@rayservice.com>
2025-07-02 09:40:14 +0200

    Create Tabx_RS_PrehledKapacitRSSouhrn.sql

---
+++ Tabx_RS_PrehledKapacitRSSouhrn.sql
@@ -0,0 +1,114 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_PrehledKapacitRSSouhrn]    Script Date: 02.07.2025 9:40:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Leden] [numeric](19, 6) NULL,
+	[Autor] [nvarchar](128) NOT NULL,
+	[DatPorizeni] [datetime] NOT NULL,
+	[Zmenil] [nvarchar](128) NULL,
+	[DatZmeny] [datetime] NULL,
+	[BlokovaniEditoru] [smallint] NULL,
+	[DatPorizeni_D]  AS (datepart(day,[DatPorizeni])),
+	[DatPorizeni_M]  AS (datepart(month,[DatPorizeni])),
+	[DatPorizeni_Y]  AS (datepart(year,[DatPorizeni])),
+	[DatPorizeni_Q]  AS (datepart(quarter,[DatPorizeni])),
+	[DatPorizeni_W]  AS (datepart(week,[DatPorizeni])),
+	[DatPorizeni_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatPorizeni])))),
+	[DatZmeny_D]  AS (datepart(day,[DatZmeny])),
+	[DatZmeny_M]  AS (datepart(month,[DatZmeny])),
+	[DatZmeny_Y]  AS (datepart(year,[DatZmeny])),
+	[DatZmeny_Q]  AS (datepart(quarter,[DatZmeny])),
+	[DatZmeny_W]  AS (datepart(week,[DatZmeny])),
+	[DatZmeny_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatZmeny])))),
+	[Unor] [numeric](19, 6) NULL,
+	[Brezen] [numeric](19, 6) NULL,
+	[Duben] [numeric](19, 6) NULL,
+	[Kveten] [numeric](19, 6) NULL,
+	[Cerven] [numeric](19, 6) NULL,
+	[Cervenec] [numeric](19, 6) NULL,
+	[Srpen] [numeric](19, 6) NULL,
+	[Zari] [numeric](19, 6) NULL,
+	[Rijen] [numeric](19, 6) NULL,
+	[Listopad] [numeric](19, 6) NULL,
+	[Prosinec] [numeric](19, 6) NULL,
+	[TypKapacity] [int] NOT NULL,
+	[Konstanta] [int] NULL,
+	[Prodano] [int] NULL,
+	[TotalKapacityPercentage] [numeric](19, 6) NULL,
+	[KonstantaDisponibility] [int] NULL,
+	[TotalKapacity] [numeric](19, 6) NULL,
+ CONSTRAINT [PK__Tabx_RS_PrehledKapacitRSSouhrn__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Leden]  DEFAULT ((0)) FOR [Leden]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Autor]  DEFAULT (suser_sname()) FOR [Autor]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__DatPorizeni]  DEFAULT (getdate()) FOR [DatPorizeni]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Unor]  DEFAULT ((0)) FOR [Unor]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Brezen]  DEFAULT ((0)) FOR [Brezen]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Duben]  DEFAULT ((0)) FOR [Duben]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Kveten]  DEFAULT ((0)) FOR [Kveten]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Cerven]  DEFAULT ((0)) FOR [Cerven]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Cervenec]  DEFAULT ((0)) FOR [Cervenec]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Srpen]  DEFAULT ((0)) FOR [Srpen]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Zari]  DEFAULT ((0)) FOR [Zari]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Rijen]  DEFAULT ((0)) FOR [Rijen]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Listopad]  DEFAULT ((0)) FOR [Listopad]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Prosinec]  DEFAULT ((0)) FOR [Prosinec]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__TypKapacity]  DEFAULT ((0)) FOR [TypKapacity]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Konstanta]  DEFAULT ((0)) FOR [Konstanta]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__Prodano]  DEFAULT ((0)) FOR [Prodano]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__TotalKapacityPercentage]  DEFAULT ((0)) FOR [TotalKapacityPercentage]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__KonstantaDisponibility]  DEFAULT ((0)) FOR [KonstantaDisponibility]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_PrehledKapacitRSSouhrn] ADD  CONSTRAINT [DF__Tabx_RS_PrehledKapacitRSSouhrn__TotalKapacity]  DEFAULT ((0)) FOR [TotalKapacity]
+GO
+
