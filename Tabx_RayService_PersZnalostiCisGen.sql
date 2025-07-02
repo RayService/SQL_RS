fe6320860c1f03ea423e24d869aea89e15bf7bc0
RayService <michal.zufan@rayservice.com>
2025-07-02 08:52:39 +0200

    Create Tabx_RayService_PersZnalostiCisGen.sql

---
+++ Tabx_RayService_PersZnalostiCisGen.sql
@@ -0,0 +1,29 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RayService_PersZnalostiCisGen]    Script Date: 02.07.2025 8:52:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RayService_PersZnalostiCisGen](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Typ] [tinyint] NOT NULL,
+	[Ident] [int] NOT NULL,
+	[Operace] [nchar](4) NULL,
+	[IDPersZnalostiCis] [int] NULL,
+ CONSTRAINT [PK__Tabx_RayService_PersZnalostiCisGen__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ__Tabx_RayService_PersZnalostiCisGen__Typ__Ident__Operace] UNIQUE NONCLUSTERED 
+(
+	[Typ] ASC,
+	[Ident] ASC,
+	[Operace] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
