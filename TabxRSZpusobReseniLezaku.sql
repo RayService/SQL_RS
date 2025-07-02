6e2e89428950227ba371de1b05adad2d8d58f3dc
RayService <michal.zufan@rayservice.com>
2025-07-02 10:47:27 +0200

    Create TabxRSZpusobReseniLezaku.sql

---
+++ TabxRSZpusobReseniLezaku.sql
@@ -0,0 +1,43 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[TabxRSZpusobReseniLezaku]    Script Date: 02.07.2025 10:47:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TabxRSZpusobReseniLezaku](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Zpusob] [nvarchar](50) NOT NULL,
+	[Autor] [nvarchar](128) NOT NULL,
+	[DatPorizeni] [datetime] NOT NULL,
+	[Zmenil] [nvarchar](128) NULL,
+	[DatZmeny] [datetime] NULL,
+	[BlokovaniEditoru] [smallint] NULL,
+	[DatPorizeni_D]  AS (datepart(day,[DatPorizeni])),
+	[DatPorizeni_M]  AS (datepart(month,[DatPorizeni])),
+	[DatPorizeni_Y]  AS (datepart(year,[DatPorizeni])),
+	[DatPorizeni_Q]  AS (datepart(quarter,[DatPorizeni])),
+	[DatPorizeni_W]  AS (datepart(week,[DatPorizeni])),
+	[DatPorizeni_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatPorizeni])))),
+	[DatZmeny_D]  AS (datepart(day,[DatZmeny])),
+	[DatZmeny_M]  AS (datepart(month,[DatZmeny])),
+	[DatZmeny_Y]  AS (datepart(year,[DatZmeny])),
+	[DatZmeny_Q]  AS (datepart(quarter,[DatZmeny])),
+	[DatZmeny_W]  AS (datepart(week,[DatZmeny])),
+	[DatZmeny_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatZmeny])))),
+ CONSTRAINT [PK__TabxRSZpusobReseniLezaku__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TabxRSZpusobReseniLezaku] ADD  CONSTRAINT [DF__TabxRSZpusobReseniLezaku__Autor]  DEFAULT (suser_sname()) FOR [Autor]
+GO
+
+ALTER TABLE [dbo].[TabxRSZpusobReseniLezaku] ADD  CONSTRAINT [DF__TabxRSZpusobReseniLezaku__DatPorizeni]  DEFAULT (getdate()) FOR [DatPorizeni]
+GO
+
