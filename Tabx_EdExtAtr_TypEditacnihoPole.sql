cde62de40f2a4ab176a02dde1d6e883894daf368
RayService <michal.zufan@rayservice.com>
2025-07-02 08:43:52 +0200

    Create Tabx_EdExtAtr_TypEditacnihoPole.sql

---
+++ Tabx_EdExtAtr_TypEditacnihoPole.sql
@@ -0,0 +1,18 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_EdExtAtr_TypEditacnihoPole]    Script Date: 02.07.2025 8:43:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_EdExtAtr_TypEditacnihoPole](
+	[BrowseID] [int] NULL,
+	[TypAtr] [nvarchar](100) NULL,
+	[TypPole] [nvarchar](100) NULL,
+	[Konverze] [bit] NULL
+) ON [PRIMARY]
+GO
+
