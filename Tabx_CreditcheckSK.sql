5a2ab076893c505e2d8c3d41253e4c3f6478db66
RayService <michal.zufan@rayservice.com>
2025-07-02 08:42:04 +0200

    Create Tabx_CreditcheckSK.sql

---
+++ Tabx_CreditcheckSK.sql
@@ -0,0 +1,22 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_CreditcheckSK]    Script Date: 02.07.2025 8:42:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_CreditcheckSK](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDCc] [int] NULL,
+	[ICO] [nvarchar](20) NULL,
+	[Stav] [tinyint] NULL,
+ CONSTRAINT [PK__Tabx_CreditcheckSK__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
