bb50a78ed44f11e2bf626ce00b92c504f7388e3e
RayService <michal.zufan@rayservice.com>
2025-07-02 10:23:02 +0200

    Create Tabx_SDErrors.sql

---
+++ Tabx_SDErrors.sql
@@ -0,0 +1,29 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_SDErrors]    Script Date: 02.07.2025 10:22:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_SDErrors](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[SPID] [int] NOT NULL,
+	[LoginID] [nvarchar](128) NOT NULL,
+	[TypHlasky] [tinyint] NOT NULL,
+	[TextHlasky] [nvarchar](max) NULL,
+ CONSTRAINT [PK__Tabx_SDErrors__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tabx_SDErrors] ADD  CONSTRAINT [DF__Tabx_SDErrors__SPID]  DEFAULT ((0)) FOR [SPID]
+GO
+
+ALTER TABLE [dbo].[Tabx_SDErrors] ADD  CONSTRAINT [DF__Tabx_SDErrors__TypHlasky]  DEFAULT ((0)) FOR [TypHlasky]
+GO
+
