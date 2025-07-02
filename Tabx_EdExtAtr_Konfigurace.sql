a9b90108620ceeea4f27cb27373f7cb746c54499
RayService <michal.zufan@rayservice.com>
2025-07-02 08:42:59 +0200

    Create Tabx_EdExtAtr_Konfigurace.sql

---
+++ Tabx_EdExtAtr_Konfigurace.sql
@@ -0,0 +1,15 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_EdExtAtr_Konfigurace]    Script Date: 02.07.2025 8:42:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_EdExtAtr_Konfigurace](
+	[VerzeHeO] [bigint] NULL
+) ON [PRIMARY]
+GO
+
