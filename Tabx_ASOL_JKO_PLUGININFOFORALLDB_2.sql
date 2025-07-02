22bde812f765f13af2d859d5f4d2ad103e94dfcc
RayService <michal.zufan@rayservice.com>
2025-07-02 08:23:52 +0200

    Create Tabx_ASOL_JKO_PLUGININFOFORALLDB_2.sql

---
+++ Tabx_ASOL_JKO_PLUGININFOFORALLDB_2.sql
@@ -0,0 +1,28 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_ASOL_JKO_PLUGININFOFORALLDB_2]    Script Date: 02.07.2025 8:23:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_ASOL_JKO_PLUGININFOFORALLDB_2](
+	[plugin] [nvarchar](60) NULL,
+	[RayService] [nvarchar](51) NULL,
+	[LTV] [nvarchar](51) NULL,
+	[RSdefence] [nvarchar](51) NULL,
+	[EWAS] [nvarchar](51) NULL,
+	[HCvicna] [nvarchar](51) NULL,
+	[RayServiceKON] [nvarchar](51) NULL,
+	[RayServiceKON1] [nvarchar](51) NULL,
+	[RayServiceKON2] [nvarchar](51) NULL,
+	[RayService5] [nvarchar](51) NULL,
+	[RayServiceKON6] [nvarchar](51) NULL,
+	[RayService6] [nvarchar](51) NULL,
+	[RayServiceKON4] [nvarchar](51) NULL,
+	[RayService7] [nvarchar](51) NULL
+) ON [PRIMARY]
+GO
+
