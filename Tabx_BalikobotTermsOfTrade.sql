3f0f569b951e761d243e3209c63062195b69b09b
RayService <michal.zufan@rayservice.com>
2025-07-02 08:35:00 +0200

    Create Tabx_BalikobotTermsOfTrade.sql

---
+++ Tabx_BalikobotTermsOfTrade.sql
@@ -0,0 +1,22 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_BalikobotTermsOfTrade]    Script Date: 02.07.2025 8:34:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_BalikobotTermsOfTrade](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[KodDopravce] [nvarchar](20) NULL,
+	[Zkratka] [nvarchar](3) NOT NULL,
+	[Popis] [nvarchar](255) NULL,
+ CONSTRAINT [PK__Tabx_BalikobotTermsOfTrade__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
