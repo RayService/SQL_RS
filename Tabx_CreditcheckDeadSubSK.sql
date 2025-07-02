5f769551036e7ea44d9346779b8ad64736baf7af
RayService <michal.zufan@rayservice.com>
2025-07-02 08:40:45 +0200

    Create Tabx_CreditcheckDeadSubSK.sql

---
+++ Tabx_CreditcheckDeadSubSK.sql
@@ -0,0 +1,20 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_CreditcheckDeadSubSK]    Script Date: 02.07.2025 8:40:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_CreditcheckDeadSubSK](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ICO] [nvarchar](20) NULL,
+ CONSTRAINT [PK__Tabx_CreditcheckDeadSubSK__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
