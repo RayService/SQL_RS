c5068a7488cc7650583cfc68333c1d119c6ee94e
RayService <michal.zufan@rayservice.com>
2025-07-02 08:23:02 +0200

    Create Tabx_ASOL_JKO_DB_OK.sql

---
+++ Tabx_ASOL_JKO_DB_OK.sql
@@ -0,0 +1,15 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_ASOL_JKO_DB_OK]    Script Date: 02.07.2025 8:22:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_ASOL_JKO_DB_OK](
+	[DBName] [sysname] NOT NULL
+) ON [PRIMARY]
+GO
+
