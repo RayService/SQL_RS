bcf77e0d891948d6f5be63df77043c164ed94d73
RayService <michal.zufan@rayservice.com>
2025-07-02 09:04:31 +0200

    Create Tabx_RS_defects_operation.sql

---
+++ Tabx_RS_defects_operation.sql
@@ -0,0 +1,21 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_defects_operation]    Script Date: 02.07.2025 9:04:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_defects_operation](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDMzdy] [int] NULL,
+	[IDOperation] [int] NULL,
+ CONSTRAINT [PK__Tabx_RS_defects_operation__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 100, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
