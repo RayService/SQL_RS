1913efc247639b1244494ac9fa9fc1961ad0d3e7
RayService <michal.zufan@rayservice.com>
2025-07-02 10:15:03 +0200

    Create Tabx_RS_workplace_capacity.sql

---
+++ Tabx_RS_workplace_capacity.sql
@@ -0,0 +1,52 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_workplace_capacity]    Script Date: 02.07.2025 10:15:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_workplace_capacity](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDWorkplace] [int] NOT NULL,
+	[Datum] [datetime] NOT NULL,
+	[WorkerAmount] [numeric](5, 2) NULL,
+	[Capacity] [numeric](19, 6) NULL,
+	[Autor] [nvarchar](128) NOT NULL,
+	[DatPorizeni] [datetime] NOT NULL,
+	[Zmenil] [nvarchar](128) NULL,
+	[DatZmeny] [datetime] NULL,
+	[BlokovaniEditoru] [smallint] NULL,
+	[DatPorizeni_D]  AS (datepart(day,[DatPorizeni])),
+	[DatPorizeni_M]  AS (datepart(month,[DatPorizeni])),
+	[DatPorizeni_Y]  AS (datepart(year,[DatPorizeni])),
+	[DatPorizeni_Q]  AS (datepart(quarter,[DatPorizeni])),
+	[DatPorizeni_W]  AS (datepart(week,[DatPorizeni])),
+	[DatPorizeni_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatPorizeni])))),
+	[DatZmeny_D]  AS (datepart(day,[DatZmeny])),
+	[DatZmeny_M]  AS (datepart(month,[DatZmeny])),
+	[DatZmeny_Y]  AS (datepart(year,[DatZmeny])),
+	[DatZmeny_Q]  AS (datepart(quarter,[DatZmeny])),
+	[DatZmeny_W]  AS (datepart(week,[DatZmeny])),
+	[DatZmeny_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatZmeny])))),
+ CONSTRAINT [PK__Tabx_RS_workplace_capacity__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_workplace_capacity] ADD  CONSTRAINT [DF__Tabx_RS_workplace_capacity__WorkerAmount]  DEFAULT ((0)) FOR [WorkerAmount]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_workplace_capacity] ADD  CONSTRAINT [DF__Tabx_RS_workplace_capacity__Capacity]  DEFAULT ((0)) FOR [Capacity]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_workplace_capacity] ADD  CONSTRAINT [DF__Tabx_RS_workplace_capacity__Autor]  DEFAULT (suser_sname()) FOR [Autor]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_workplace_capacity] ADD  CONSTRAINT [DF__Tabx_RS_workplace_capacity__DatPorizeni]  DEFAULT (getdate()) FOR [DatPorizeni]
+GO
+
