aa048127cdc9e3e1f08c40d03ab4d94258f47572
RayService <michal.zufan@rayservice.com>
2025-07-02 08:22:25 +0200

    Create Tabx_ASOL_JKO_DB.sql

---
+++ Tabx_ASOL_JKO_DB.sql
@@ -0,0 +1,48 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_ASOL_JKO_DB]    Script Date: 02.07.2025 8:22:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_ASOL_JKO_DB](
+	[DATABASEID] [int] NULL,
+	[___Legislativa___] [nvarchar](50) NULL,
+	[___VerzeHelios___] [nvarchar](50) NULL,
+	[plgASOLHromEditExtAtr] [nvarchar](50) NULL,
+	[plgASOLRayServiceGenerovaniFP] [nvarchar](50) NULL,
+	[PlugGatema27Tracebilita] [nvarchar](50) NULL,
+	[PlugGatema8Kardex] [nvarchar](50) NULL,
+	[PlugGatemaFakturyExp] [nvarchar](50) NULL,
+	[PlugGatemaInventuraMajetku] [nvarchar](50) NULL,
+	[PlugGatemaLogovaniZmen] [nvarchar](50) NULL,
+	[PlugGatemaSDMaterial] [nvarchar](50) NULL,
+	[PluginAresOvereni] [nvarchar](50) NULL,
+	[PluginBanka0100] [nvarchar](50) NULL,
+	[PluginBanka0900] [nvarchar](50) NULL,
+	[PluginBanka2200] [nvarchar](50) NULL,
+	[PluginBankaSepaXMLu] [nvarchar](50) NULL,
+	[PluginPreneseniDanPovinnosti] [nvarchar](50) NULL,
+	[PluginRemotePrint] [nvarchar](50) NULL,
+	[PluginStavUmisteni] [nvarchar](50) NULL,
+	[rpB2Aemployeerating] [nvarchar](50) NULL,
+	[rpB2Ameasuredvalues] [nvarchar](50) NULL,
+	[rpB2Atooldatamanagement] [nvarchar](50) NULL,
+	[rpCreditcheck] [nvarchar](50) NULL,
+	[rpGadgets] [nvarchar](50) NULL,
+	[rpHCVObchKalk] [nvarchar](50) NULL,
+	[rpIcExportRAVProKB] [nvarchar](50) NULL,
+	[rpICImportPlanuZExcelu] [nvarchar](50) NULL,
+	[rpImportKL] [nvarchar](50) NULL,
+	[rpISDOC] [nvarchar](50) NULL,
+	[rpNahradniPlneni] [nvarchar](50) NULL,
+	[rpScan] [nvarchar](50) NULL,
+	[rpSchvalovaniDokladu] [nvarchar](50) NULL,
+	[rpVyuctovaniZalDane] [nvarchar](50) NULL,
+	[RwEasyMAPI64_installer] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+
