3b84681c72c4e333c379addc0ec7c190ab9e17c2
RayService <michal.zufan@rayservice.com>
2025-07-02 10:11:55 +0200

    Create Tabx_RS_UzkaMistaVyroby.sql

---
+++ Tabx_RS_UzkaMistaVyroby.sql
@@ -0,0 +1,56 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_UzkaMistaVyroby]    Script Date: 02.07.2025 10:11:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_UzkaMistaVyroby](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDPrikaz] [int] NOT NULL,
+	[ObratPolozky] [numeric](19, 6) NULL,
+	[Typ] [nvarchar](10) NULL,
+	[DruhMista] [int] NOT NULL,
+	[DatumVypoctu] [datetime] NOT NULL,
+	[DatumVypoctu_D]  AS (datepart(day,[DatumVypoctu])),
+	[DatumVypoctu_M]  AS (datepart(month,[DatumVypoctu])),
+	[DatumVypoctu_Y]  AS (datepart(year,[DatumVypoctu])),
+	[DatumVypoctu_Q]  AS (datepart(quarter,[DatumVypoctu])),
+	[DatumVypoctu_W]  AS (datepart(week,[DatumVypoctu])),
+	[DatumVypoctu_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatumVypoctu])))),
+	[Autor] [nvarchar](128) NOT NULL,
+	[DatPorizeni] [datetime] NOT NULL,
+	[Zmenil] [nvarchar](128) NULL,
+	[DatZmeny] [datetime] NULL,
+	[BlokovaniEditoru] [smallint] NULL,
+	[DatPorizeni_D]  AS (datepart(day,[DatPorizeni])),
+	[DatPorizeni_M]  AS (datepart(month,[DatPorizeni])),
+	[DatPorizeni_Y]  AS (datepart(year,[DatPorizeni])),
+	[DatPorizeni_Q]  AS (datepart(quarter,[DatPorizeni])),
+	[DatPorizeni_W]  AS (datepart(week,[DatPorizeni])),
+	[DatPorizeni_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatPorizeni])))),
+	[DatZmeny_D]  AS (datepart(day,[DatZmeny])),
+	[DatZmeny_M]  AS (datepart(month,[DatZmeny])),
+	[DatZmeny_Y]  AS (datepart(year,[DatZmeny])),
+	[DatZmeny_Q]  AS (datepart(quarter,[DatZmeny])),
+	[DatZmeny_W]  AS (datepart(week,[DatZmeny])),
+	[DatZmeny_X]  AS (CONVERT([datetime],CONVERT([int],CONVERT([float],[DatZmeny])))),
+ CONSTRAINT [PK__Tabx_RS_UzkaMistaVyroby__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_UzkaMistaVyroby] ADD  CONSTRAINT [DF__Tabx_RS_UzkaMistaVyroby__ObratPolozky]  DEFAULT ((0)) FOR [ObratPolozky]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_UzkaMistaVyroby] ADD  CONSTRAINT [DF__Tabx_RS_UzkaMistaVyroby__Autor]  DEFAULT (suser_sname()) FOR [Autor]
+GO
+
+ALTER TABLE [dbo].[Tabx_RS_UzkaMistaVyroby] ADD  CONSTRAINT [DF__Tabx_RS_UzkaMistaVyroby__DatPorizeni]  DEFAULT (getdate()) FOR [DatPorizeni]
+GO
+
