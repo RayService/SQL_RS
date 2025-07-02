5dfc8fcdab09a90c4116dc1939f08f403b7a9c13
RayService <michal.zufan@rayservice.com>
2025-07-02 10:18:15 +0200

    Create Tabx_RSPersMzdy_PozadovaneDovednosti.sql

---
+++ Tabx_RSPersMzdy_PozadovaneDovednosti.sql
@@ -0,0 +1,29 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RSPersMzdy_PozadovaneDovednosti]    Script Date: 02.07.2025 10:18:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RSPersMzdy_PozadovaneDovednosti](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDZam] [int] NOT NULL,
+	[Vypocet] [smallint] NOT NULL,
+	[IDPPDetailZnalosti] [int] NOT NULL,
+	[IDPP] [int] NOT NULL,
+ CONSTRAINT [PK__Tabx_RSPersMzdy_PozadovaneDovednosti__ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ__Tabx_RSPersMzdy_PozadovaneDovednosti__IDZam__Vypocet__IDPPDetailZnalosti] UNIQUE NONCLUSTERED 
+(
+	[IDZam] ASC,
+	[Vypocet] ASC,
+	[IDPPDetailZnalosti] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
