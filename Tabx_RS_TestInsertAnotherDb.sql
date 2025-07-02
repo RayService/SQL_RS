968bfbece9d91fb81871315c64aed64f3ebf1763
RayService <michal.zufan@rayservice.com>
2025-07-02 10:02:24 +0200

    Create Tabx_RS_TestInsertAnotherDb.sql

---
+++ Tabx_RS_TestInsertAnotherDb.sql
@@ -0,0 +1,16 @@
+USE [RayService]
+GO
+
+/****** Object:  Table [dbo].[Tabx_RS_TestInsertAnotherDb]    Script Date: 02.07.2025 10:02:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tabx_RS_TestInsertAnotherDb](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Value] [int] NULL
+) ON [PRIMARY]
+GO
+
