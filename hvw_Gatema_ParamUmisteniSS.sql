2b315120c7999041255794c0ee41b471e31ee9b3
RayService <michal.zufan@rayservice.com>
2025-07-03 15:05:00 +0200

    Create hvw_Gatema_ParamUmisteniSS.sql

---
+++ hvw_Gatema_ParamUmisteniSS.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_ParamUmisteniSS]    Script Date: 03.07.2025 15:04:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_ParamUmisteniSS] AS SELECT * FROM Gatema_ParamUmisteniSS
+GO
+
