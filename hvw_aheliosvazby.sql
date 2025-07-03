036e71cbaabab3c698fe1fbbd640dc280b45c2dc
RayService <michal.zufan@rayservice.com>
2025-07-03 12:55:01 +0200

    Create hvw_aheliosvazby.sql

---
+++ hvw_aheliosvazby.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_aheliosvazby]    Script Date: 03.07.2025 12:52:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_aheliosvazby] AS select * from aheliosvazby
+GO
+
