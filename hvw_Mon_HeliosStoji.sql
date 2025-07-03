ab3e6e923cde3d6e4be4d07a43b10e09cc5401e1
RayService <michal.zufan@rayservice.com>
2025-07-03 15:26:33 +0200

    Create hvw_Mon_HeliosStoji.sql

---
+++ hvw_Mon_HeliosStoji.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Mon_HeliosStoji]    Script Date: 03.07.2025 15:26:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Mon_HeliosStoji] AS SELECT*FROM Tabx_Mon_HeliosStoji
+GO
+
