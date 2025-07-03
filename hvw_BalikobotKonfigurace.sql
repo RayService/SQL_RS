3e425e8aebe2aa4a9d1891cdb67c0d6d8e72b041
RayService <michal.zufan@rayservice.com>
2025-07-03 14:00:38 +0200

    Create hvw_BalikobotKonfigurace.sql

---
+++ hvw_BalikobotKonfigurace.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotKonfigurace]    Script Date: 03.07.2025 13:59:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotKonfigurace] AS SELECT * FROM Tabx_BalikobotKonfigurace
+GO
+
