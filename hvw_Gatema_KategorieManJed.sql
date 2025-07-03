56a90ee38c2a0427b19e645325cae013506aeeda
RayService <michal.zufan@rayservice.com>
2025-07-03 15:02:08 +0200

    Create hvw_Gatema_KategorieManJed.sql

---
+++ hvw_Gatema_KategorieManJed.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_KategorieManJed]    Script Date: 03.07.2025 15:01:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_KategorieManJed] AS SELECT * FROM Gatema_KategorieManJed
+GO
+
