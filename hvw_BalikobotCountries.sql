2abd9497f969cb0f2b85a9d76303aed776e7cd85
RayService <michal.zufan@rayservice.com>
2025-07-03 13:54:28 +0200

    Create hvw_BalikobotCountries.sql

---
+++ hvw_BalikobotCountries.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotCountries]    Script Date: 03.07.2025 13:54:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotCountries] AS SELECT * FROM Tabx_BalikobotCountries
+GO
+
