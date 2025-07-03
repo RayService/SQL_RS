724eb5e5f90da66d2f022ba3ecc7909886c83a97
RayService <michal.zufan@rayservice.com>
2025-07-03 14:37:36 +0200

    Create hvw_DocSkenKonfig.sql

---
+++ hvw_DocSkenKonfig.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_DocSkenKonfig]    Script Date: 03.07.2025 14:37:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_DocSkenKonfig] AS SELECT * FROM TabDocSkenKonfig
+GO
+
