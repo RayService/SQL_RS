e50a547b23009e133746d3c8d54c5401923a07ff
RayService <michal.zufan@rayservice.com>
2025-07-03 08:28:55 +0200

    Create et_TabUniImportDVZ.sql

---
+++ et_TabUniImportDVZ.sql
@@ -0,0 +1,33 @@
+USE [HCvicna]
+GO
+
+/****** Object:  Trigger [dbo].[et_TabUniImportDVZ]    Script Date: 03.07.2025 8:28:49 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*rpObecneImporty.Import*/CREATE TRIGGER [dbo].[et_TabUniImportDVZ] ON [dbo].[TabUniImportDVZ] FOR UPDATE
+AS
+IF NOT UPDATE(Chyba) AND NOT UPDATE(DatumImportu)
+BEGIN
+DECLARE @ID INT, @DatumImportu DATETIME
+DECLARE c CURSOR LOCAL FAST_FORWARD FOR
+SELECT ID, DatumImportu FROM INSERTED
+OPEN c
+FETCH NEXT FROM c INTO @ID, @DatumImportu
+WHILE 1=1
+BEGIN
+IF @DatumImportu IS NOT NULL UPDATE TabUniImportDVZ SET Chyba=NULL, DatumImportu=NULL WHERE ID=@ID
+FETCH NEXT FROM c INTO @ID, @DatumImportu
+IF @@fetch_status<>0 BREAK
+END
+CLOSE c
+DEALLOCATE c
+END
+GO
+
+ALTER TABLE [dbo].[TabUniImportDVZ] ENABLE TRIGGER [et_TabUniImportDVZ]
+GO
+
