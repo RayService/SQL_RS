aa518c5fb9929950dd572271cf94ea2852d1c53f
RayService <michal.zufan@rayservice.com>
2025-07-03 15:20:07 +0200

    Create hvw_Kardex.sql

---
+++ hvw_Kardex.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Kardex]    Script Date: 03.07.2025 15:20:03 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Kardex] AS SELECT * FROM GTabKardex
+GO
+
