78f57d21c9502886ee33dc0ddd15f43dc7ebda92
RayService <michal.zufan@rayservice.com>
2025-07-03 15:01:17 +0200

    Create hvw_Gatema_InvItemUmisteni.sql

---
+++ hvw_Gatema_InvItemUmisteni.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_InvItemUmisteni]    Script Date: 03.07.2025 15:01:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_InvItemUmisteni] AS SELECT * FROM Gatema_InvItemUmisteni
+GO
+
