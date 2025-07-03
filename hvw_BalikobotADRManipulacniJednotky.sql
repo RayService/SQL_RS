c8290fcc09e226b5276561686bedec3688d1d587
RayService <michal.zufan@rayservice.com>
2025-07-03 13:52:05 +0200

    Create hvw_BalikobotADRManipulacniJednotky.sql

---
+++ hvw_BalikobotADRManipulacniJednotky.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotADRManipulacniJednotky]    Script Date: 03.07.2025 13:52:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotADRManipulacniJednotky] AS SELECT * FROM Tabx_BalikobotADRManipulacniJednotky
+GO
+
