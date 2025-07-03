2e2f01642f7ce6a87ac1eb3d52d7c51082686e27
RayService <michal.zufan@rayservice.com>
2025-07-03 14:07:01 +0200

    Create hvw_BalikobotUPSAPID.sql

---
+++ hvw_BalikobotUPSAPID.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotUPSAPID]    Script Date: 03.07.2025 14:06:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotUPSAPID] AS SELECT * FROM Tabx_BalikobotUPSAPID
+GO
+
