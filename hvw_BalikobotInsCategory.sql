8f8a9236e7038e71ca1bb3b83258805eb536461c
RayService <michal.zufan@rayservice.com>
2025-07-03 13:57:48 +0200

    Create hvw_BalikobotInsCategory.sql

---
+++ hvw_BalikobotInsCategory.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotInsCategory]    Script Date: 03.07.2025 13:57:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotInsCategory] AS SELECT * FROM Tabx_BalikobotInsCategory
+GO
+
