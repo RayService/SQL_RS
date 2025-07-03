1bd871c2baa9d6d005d19ce781484a0d0f453a10
RayService <michal.zufan@rayservice.com>
2025-07-03 13:54:47 +0200

    Create hvw_BalikobotCountriesFilter.sql

---
+++ hvw_BalikobotCountriesFilter.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotCountriesFilter]    Script Date: 03.07.2025 13:54:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotCountriesFilter] AS SELECT*FROM Tabx_BalikobotCountriesFilter
+GO
+
