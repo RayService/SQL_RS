17dabf5cb9449a3400bda0888e9210b2d5765525
RayService <michal.zufan@rayservice.com>
2025-07-03 15:06:48 +0200

    Create hvw_Gatema_PohybManJedStruk.sql

---
+++ hvw_Gatema_PohybManJedStruk.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_PohybManJedStruk]    Script Date: 03.07.2025 15:06:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_PohybManJedStruk] AS SELECT * FROM Gatema_PohybManJedStruk
+GO
+
