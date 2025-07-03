e86ab78375c524b4f7b33687e61c034153aa3a7f
RayService <michal.zufan@rayservice.com>
2025-07-03 12:38:22 +0200

    Create hvw_8SappertaTabDeffOken.sql

---
+++ hvw_8SappertaTabDeffOken.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_8SappertaTabDeffOken]    Script Date: 03.07.2025 12:38:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_8SappertaTabDeffOken] AS select * from Saperta_Tab_DefOken
+GO
+
