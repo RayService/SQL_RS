56a6ef41e642b0ea11751ed38d7d7b1ea57536dd
RayService <michal.zufan@rayservice.com>
2025-07-03 15:03:31 +0200

    Create hvw_Gatema_ManJedPol.sql

---
+++ hvw_Gatema_ManJedPol.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_ManJedPol]    Script Date: 03.07.2025 15:03:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_ManJedPol] AS SELECT * FROM Gatema_ManJedPol
+GO
+
