1591ad48e5adc8436480dbf6b1078c37693c9bd5
RayService <michal.zufan@rayservice.com>
2025-07-03 12:43:15 +0200

    Create hvw_93sapkopirovaniizaznamu.sql

---
+++ hvw_93sapkopirovaniizaznamu.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_93sapkopirovaniizaznamu]    Script Date: 03.07.2025 12:43:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_93sapkopirovaniizaznamu] AS select * from atabukopiezaznamu
+GO
+
