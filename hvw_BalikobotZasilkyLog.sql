8d8c35e496f5508bbed4220250f1d16624965b08
RayService <michal.zufan@rayservice.com>
2025-07-03 14:10:43 +0200

    Create hvw_BalikobotZasilkyLog.sql

---
+++ hvw_BalikobotZasilkyLog.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotZasilkyLog]    Script Date: 03.07.2025 14:10:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotZasilkyLog] AS SELECT * FROM Tabx_BalikobotZasilkyLog
+GO
+
