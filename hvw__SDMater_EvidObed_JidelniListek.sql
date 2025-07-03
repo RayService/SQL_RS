f369058927fbe678e3ed951a802a58dd0ca2b13c
RayService <michal.zufan@rayservice.com>
2025-07-03 10:49:56 +0200

    Create hvw__SDMater_EvidObed_JidelniListek.sql

---
+++ hvw__SDMater_EvidObed_JidelniListek.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw__SDMater_EvidObed_JidelniListek]    Script Date: 03.07.2025 10:49:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw__SDMater_EvidObed_JidelniListek] AS SELECT * FROM Tab_SDMater_EvidObed_JidelniListek
+GO
+
