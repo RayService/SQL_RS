3978f73033019808a6497ee5f7106a1cb8d6ee46
RayService <michal.zufan@rayservice.com>
2025-07-03 15:15:57 +0200

    Create hvw_GLogovaneInformace.sql

---
+++ hvw_GLogovaneInformace.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_GLogovaneInformace]    Script Date: 03.07.2025 15:15:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_GLogovaneInformace] AS SELECT *  FROM GTabLogovaneInformace
+GO
+
