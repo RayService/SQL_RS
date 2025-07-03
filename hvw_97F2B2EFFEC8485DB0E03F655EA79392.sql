ba0c716eb52ebe54efd21d0e487bfcafc16b32f6
RayService <michal.zufan@rayservice.com>
2025-07-03 12:44:48 +0200

    Create hvw_97F2B2EFFEC8485DB0E03F655EA79392.sql

---
+++ hvw_97F2B2EFFEC8485DB0E03F655EA79392.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_97F2B2EFFEC8485DB0E03F655EA79392]    Script Date: 03.07.2025 12:44:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_97F2B2EFFEC8485DB0E03F655EA79392] AS select * from aparam
+GO
+
