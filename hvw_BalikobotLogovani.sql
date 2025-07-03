ab82f05a9221f6fe7524c1fdf580695b69fcec99
RayService <michal.zufan@rayservice.com>
2025-07-03 14:01:26 +0200

    Create hvw_BalikobotLogovani.sql

---
+++ hvw_BalikobotLogovani.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotLogovani]    Script Date: 03.07.2025 14:01:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotLogovani] AS SELECT * FROM Tabx_BalikobotLogovani
+GO
+
