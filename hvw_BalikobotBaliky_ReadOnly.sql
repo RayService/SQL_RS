ffae1ce981999d2eac7f67ec5f7a51b863db4f98
RayService <michal.zufan@rayservice.com>
2025-07-03 13:52:59 +0200

    Create hvw_BalikobotBaliky_ReadOnly.sql

---
+++ hvw_BalikobotBaliky_ReadOnly.sql
@@ -0,0 +1,14 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotBaliky_ReadOnly]    Script Date: 03.07.2025 13:52:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotBaliky_ReadOnly] AS SELECT *
+FROM Tabx_BalikobotBaliky
+GO
+
