89527160bbb317984485d10528a1e14494c1bca6
RayService <michal.zufan@rayservice.com>
2025-07-03 11:26:12 +0200

    Create hvw_851FDB753970484CAB91A84F6F2CDA45.sql

---
+++ hvw_851FDB753970484CAB91A84F6F2CDA45.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_851FDB753970484CAB91A84F6F2CDA45]    Script Date: 03.07.2025 11:26:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_851FDB753970484CAB91A84F6F2CDA45] AS select * from tabstavskladu
+GO
+
