a551601e59484322da658abcaa4815f2c65056d7
RayService <michal.zufan@rayservice.com>
2025-07-03 14:27:33 +0200

    Create hvw_CisKoduPDP.sql

---
+++ hvw_CisKoduPDP.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_CisKoduPDP]    Script Date: 03.07.2025 14:27:30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_CisKoduPDP] AS SELECT * FROM TabCisKoduPDP
+GO
+
