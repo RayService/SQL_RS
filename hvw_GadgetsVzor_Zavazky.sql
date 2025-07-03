9e5bbb299b35e5fb5c4e837684cbc4b24391541d
RayService <michal.zufan@rayservice.com>
2025-07-03 14:59:46 +0200

    Create hvw_GadgetsVzor_Zavazky.sql

---
+++ hvw_GadgetsVzor_Zavazky.sql
@@ -0,0 +1,23 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_GadgetsVzor_Zavazky]    Script Date: 03.07.2025 14:59:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_GadgetsVzor_Zavazky] AS SELECT
+REPLACE(
+REPLACE(
+CONVERT(NVARCHAR(255), CAST(ISNULL(SUM(Saldo_Ucet),0) AS MONEY), 1)
+,',',' '
+)
+,'.',','
+) + ' ' + CASE WHEN (SELECT TOP 1 Kod FROM TabKodMen WHERE HlavniMena=1)='CZK' THEN 'Kč' ELSE (SELECT TOP 1 Kod FROM TabKodMen WHERE HlavniMena=1) END
+AS Zavazky
+FROM dbo.TabSaldo
+WHERE CisloSalSk='100'
+GO
+
