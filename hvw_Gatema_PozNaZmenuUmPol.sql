aa68bbaa4e7873bb8708dad8aedea68aa070b28e
RayService <michal.zufan@rayservice.com>
2025-07-03 15:09:41 +0200

    Create hvw_Gatema_PozNaZmenuUmPol.sql

---
+++ hvw_Gatema_PozNaZmenuUmPol.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_PozNaZmenuUmPol]    Script Date: 03.07.2025 15:09:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_PozNaZmenuUmPol] AS SELECT * FROM Gatema_PozNaZmenuUmPol
+GO
+
