2fb6dd87dff63e65d49700f5ecb18bb727e1a2e3
RayService <michal.zufan@rayservice.com>
2025-07-03 13:45:25 +0200

    Create hvw_B2aMeasuredValueCategory.sql

---
+++ hvw_B2aMeasuredValueCategory.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_B2aMeasuredValueCategory]    Script Date: 03.07.2025 13:44:51 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_B2aMeasuredValueCategory] AS SELECT * FROM B2A_MeasuredValueCategory
+GO
+
