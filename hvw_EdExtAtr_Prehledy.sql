e3cc241a106edf331b07b5a263d198c3dccb50f2
RayService <michal.zufan@rayservice.com>
2025-07-03 14:43:45 +0200

    Create hvw_EdExtAtr_Prehledy.sql

---
+++ hvw_EdExtAtr_Prehledy.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_EdExtAtr_Prehledy]    Script Date: 03.07.2025 14:43:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_EdExtAtr_Prehledy] AS SELECT * FROM Tabx_EdExtAtr_Prehledy
+GO
+
