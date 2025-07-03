9c5c8a8869d8a813adb5ac20fc4baef04fc9b7b6
RayService <michal.zufan@rayservice.com>
2025-07-03 15:18:31 +0200

    Create hvw_HlavickaPDP.sql

---
+++ hvw_HlavickaPDP.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_HlavickaPDP]    Script Date: 03.07.2025 15:17:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_HlavickaPDP] AS SELECT * FROM TabHlavickaPDP
+GO
+
