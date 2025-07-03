b39e0f442ec44fd83b1f249288b958a6ee2bfcc4
RayService <michal.zufan@rayservice.com>
2025-07-03 14:55:58 +0200

    Create hvw_GadgetsDef.sql

---
+++ hvw_GadgetsDef.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_GadgetsDef]    Script Date: 03.07.2025 14:55:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_GadgetsDef] AS SELECT *FROM TabGadgetsDef
+GO
+
