23a17daef7570673d95675454627513374a0398c
RayService <michal.zufan@rayservice.com>
2025-07-03 15:13:49 +0200

    Create hvw_Gatema_UmisSkladZony.sql

---
+++ hvw_Gatema_UmisSkladZony.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Gatema_UmisSkladZony]    Script Date: 03.07.2025 15:13:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Gatema_UmisSkladZony] AS SELECT * FROM Gatema_UmisSkladZony
+GO
+
