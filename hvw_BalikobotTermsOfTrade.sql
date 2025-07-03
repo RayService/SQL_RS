ba72eef9325f6a2d4c53482886b7f18f4a467259
RayService <michal.zufan@rayservice.com>
2025-07-03 14:06:07 +0200

    Create hvw_BalikobotTermsOfTrade.sql

---
+++ hvw_BalikobotTermsOfTrade.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_BalikobotTermsOfTrade]    Script Date: 03.07.2025 14:06:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_BalikobotTermsOfTrade] AS SELECT * FROM Tabx_BalikobotTermsOfTrade
+GO
+
