1aa1cad35a8365a4ae074a5c0308f5fa556250ed
RayService <michal.zufan@rayservice.com>
2025-07-03 15:16:33 +0200

    Create hvw_GTabInvMajKonfigurace.sql

---
+++ hvw_GTabInvMajKonfigurace.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_GTabInvMajKonfigurace]    Script Date: 03.07.2025 15:16:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_GTabInvMajKonfigurace] AS SELECT * FROM GTabInvMajKonfigurace
+GO
+
