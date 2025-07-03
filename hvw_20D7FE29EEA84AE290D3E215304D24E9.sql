2cd9640f985ac30b5c83147c0638a1b59b90888c
RayService <michal.zufan@rayservice.com>
2025-07-03 11:00:31 +0200

    Create hvw_20D7FE29EEA84AE290D3E215304D24E9.sql

---
+++ hvw_20D7FE29EEA84AE290D3E215304D24E9.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_20D7FE29EEA84AE290D3E215304D24E9]    Script Date: 03.07.2025 11:00:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_20D7FE29EEA84AE290D3E215304D24E9] AS select id, code, name  from e_properties_group
+GO
+
