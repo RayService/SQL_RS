168978597330ee924d45c365c6dc1dbda5db85ba
RayService <michal.zufan@rayservice.com>
2025-07-03 15:19:29 +0200

    Create hvw_ISDOCConfig.sql

---
+++ hvw_ISDOCConfig.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_ISDOCConfig]    Script Date: 03.07.2025 15:19:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_ISDOCConfig] AS SELECT * FROM TabISDOCConfig
+GO
+
