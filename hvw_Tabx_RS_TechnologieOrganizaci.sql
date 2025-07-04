1042d77c984d79ac54474518b26a1d2df90a9e7e
RayService <michal.zufan@rayservice.com>
2025-07-04 08:53:38 +0200

    Create hvw_Tabx_RS_TechnologieOrganizaci.sql

---
+++ hvw_Tabx_RS_TechnologieOrganizaci.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_TechnologieOrganizaci]    Script Date: 04.07.2025 8:52:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_TechnologieOrganizaci] AS SELECT*FROM Tabx_RS_TechnologieOrganizaci
+GO
+
