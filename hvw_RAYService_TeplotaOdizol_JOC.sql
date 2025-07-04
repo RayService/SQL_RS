9846dab79b07220ed0af844d9478d8ae2c58c6c9
RayService <michal.zufan@rayservice.com>
2025-07-04 07:25:16 +0200

    Create hvw_RAYService_TeplotaOdizol_JOC.sql

---
+++ hvw_RAYService_TeplotaOdizol_JOC.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RAYService_TeplotaOdizol_JOC]    Script Date: 04.07.2025 7:25:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RAYService_TeplotaOdizol_JOC] AS SELECT '150-180' as Teplota UNION SELECT '200-240' as Teplota UNION SELECT '240-300' as Teplota UNION SELECT '320-400' as Teplota UNION SELECT '170-210' as Teplota UNION SELECT '130-160' as Teplota UNION SELECT '100-120' as Teplota UNION SELECT '290-360' as Teplota UNION SELECT '420-520' as Teplota
+GO
+
