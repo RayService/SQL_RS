d16e96c550e8891517d6a5dd1d06daeeb0c363b2
RayService <michal.zufan@rayservice.com>
2025-07-04 08:58:01 +0200

    Create hvw_UniImportExtAtr.sql

---
+++ hvw_UniImportExtAtr.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportExtAtr]    Script Date: 04.07.2025 8:57:56 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportExtAtr] AS SELECT *FROM TabUniImportExtAtr
+GO
+
