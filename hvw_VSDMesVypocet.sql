99749527f05e9ac02d527c41107db3367ddcbd1c
RayService <michal.zufan@rayservice.com>
2025-07-04 09:17:21 +0200

    Create hvw_VSDMesVypocet.sql

---
+++ hvw_VSDMesVypocet.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VSDMesVypocet]    Script Date: 04.07.2025 9:17:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VSDMesVypocet] AS SELECT * FROM Tabx_VSDMesVypocet
+GO
+
