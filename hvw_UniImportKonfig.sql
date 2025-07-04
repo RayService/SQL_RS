b6645481dedaf0f08fc5146c09e9c857b5a4a7a2
RayService <michal.zufan@rayservice.com>
2025-07-04 09:00:50 +0200

    Create hvw_UniImportKonfig.sql

---
+++ hvw_UniImportKonfig.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportKonfig]    Script Date: 04.07.2025 9:00:46 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportKonfig] AS SELECT *FROM TabUniImportKonfig
+GO
+
