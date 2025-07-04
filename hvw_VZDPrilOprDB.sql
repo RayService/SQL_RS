56602e8233c2ce27ab522ce9cedcf6d76a2dc21b
RayService <michal.zufan@rayservice.com>
2025-07-04 09:27:38 +0200

    Create hvw_VZDPrilOprDB.sql

---
+++ hvw_VZDPrilOprDB.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VZDPrilOprDB]    Script Date: 04.07.2025 9:27:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VZDPrilOprDB] AS SELECT * FROM Tabx_VZDPrilOprDB
+GO
+
