ea835b4f720613cc7b889939ddfb701a32867d3b
RayService <michal.zufan@rayservice.com>
2025-07-04 08:05:13 +0200

    Create hvw_RPExtUloziste.sql

---
+++ hvw_RPExtUloziste.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RPExtUloziste]    Script Date: 04.07.2025 8:05:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RPExtUloziste] AS SELECT * FROM GTabRPExtUloziste
+GO
+
