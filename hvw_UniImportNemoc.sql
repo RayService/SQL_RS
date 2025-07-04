9df4686a193eeeca9bdb376e9a3c5160b817c4da
RayService <michal.zufan@rayservice.com>
2025-07-04 09:02:32 +0200

    Create hvw_UniImportNemoc.sql

---
+++ hvw_UniImportNemoc.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportNemoc]    Script Date: 04.07.2025 9:02:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportNemoc] AS SELECT *FROM TabUniImportNemoc
+GO
+
