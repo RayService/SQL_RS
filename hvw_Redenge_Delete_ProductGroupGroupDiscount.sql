1cac8c06ea09d438f42d9c9883ff8de48f0ef723
RayService <michal.zufan@rayservice.com>
2025-07-04 07:37:45 +0200

    Create hvw_Redenge_Delete_ProductGroupGroupDiscount.sql

---
+++ hvw_Redenge_Delete_ProductGroupGroupDiscount.sql
@@ -0,0 +1,14 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Redenge_Delete_ProductGroupGroupDiscount]    Script Date: 04.07.2025 7:37:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Redenge_Delete_ProductGroupGroupDiscount] AS 
+SELECT Id FROM Redenge_Helios_RedengeKit_Shop_Structures_Product_DeletedProductGroupGroupDiscount
+GO
+
