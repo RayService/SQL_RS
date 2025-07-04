f3f92dbd24d330abc35fbca5c00a2e5f13b0d61c
RayService <michal.zufan@rayservice.com>
2025-07-04 08:43:11 +0200

    Create hvw_Tabx_RS_KontrolaDochazky.sql

---
+++ hvw_Tabx_RS_KontrolaDochazky.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_KontrolaDochazky]    Script Date: 04.07.2025 8:43:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_KontrolaDochazky] AS SELECT*FROM Tabx_RS_KontrolaDochazky
+GO
+
