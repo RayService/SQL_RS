7a1aeb037c6cdcf3ffda6272ea7509ec6cb2b5c7
RayService <michal.zufan@rayservice.com>
2025-07-04 08:26:01 +0200

    Create hvw_SHTools_BlokLogSber.sql

---
+++ hvw_SHTools_BlokLogSber.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_SHTools_BlokLogSber]    Script Date: 04.07.2025 8:25:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_SHTools_BlokLogSber] AS SELECT*FROM Tabx_SHTools_BlokLogSber
+GO
+
