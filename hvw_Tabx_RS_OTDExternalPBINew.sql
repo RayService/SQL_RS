5f8b462de6500ddade51d9e0c3179f9ed7da60ec
RayService <michal.zufan@rayservice.com>
2025-07-04 08:43:59 +0200

    Create hvw_Tabx_RS_OTDExternalPBINew.sql

---
+++ hvw_Tabx_RS_OTDExternalPBINew.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_OTDExternalPBINew]    Script Date: 04.07.2025 8:43:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_OTDExternalPBINew] AS SELECT*FROM Tabx_RS_OTDExternalPBINew
+GO
+
