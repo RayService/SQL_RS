79f0bfa5a99d3be49d1637f66cfde0548def49f7
RayService <michal.zufan@rayservice.com>
2025-07-04 09:20:33 +0200

    Create hvw_Vyskladneni.sql

---
+++ hvw_Vyskladneni.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Vyskladneni]    Script Date: 04.07.2025 9:19:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Vyskladneni] AS SELECT * FROM GTabVyskladneni
+GO
+
