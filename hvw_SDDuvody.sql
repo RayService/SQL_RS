00515688f25f5c6d40a24db137e42e2dfc222447
RayService <michal.zufan@rayservice.com>
2025-07-04 08:16:32 +0200

    Create hvw_SDDuvody.sql

---
+++ hvw_SDDuvody.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_SDDuvody]    Script Date: 04.07.2025 8:16:30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_SDDuvody] AS SELECT * FROM Tabx_SDDuvody
+GO
+
