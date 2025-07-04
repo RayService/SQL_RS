1b25116837edd0458a42d6472ba6bc563edccdaa
RayService <michal.zufan@rayservice.com>
2025-07-04 09:26:34 +0200

    Create hvw_VZDPrehledPlateb.sql

---
+++ hvw_VZDPrehledPlateb.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VZDPrehledPlateb]    Script Date: 04.07.2025 9:26:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VZDPrehledPlateb] AS SELECT * FROM Tabx_VZDPrehledPlateb
+GO
+
