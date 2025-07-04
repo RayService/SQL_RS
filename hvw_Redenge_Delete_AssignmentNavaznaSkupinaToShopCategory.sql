4c2342ae8bd1fa99a0ed58edb6fbd9518980274b
RayService <michal.zufan@rayservice.com>
2025-07-04 07:28:01 +0200

    Create hvw_Redenge_Delete_AssignmentNavaznaSkupinaToShopCategory.sql

---
+++ hvw_Redenge_Delete_AssignmentNavaznaSkupinaToShopCategory.sql
@@ -0,0 +1,14 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Redenge_Delete_AssignmentNavaznaSkupinaToShopCategory]    Script Date: 04.07.2025 7:27:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Redenge_Delete_AssignmentNavaznaSkupinaToShopCategory] AS 
+SELECT Id FROM Redenge_Helios_RedengeKit_Shop_AssignmentNavaznaSkupinaToShopCategory
+GO
+
