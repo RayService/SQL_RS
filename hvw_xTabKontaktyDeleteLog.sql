c4e53e24da8233ca89270093558d58b84acb3276
RayService <michal.zufan@rayservice.com>
2025-07-04 09:31:05 +0200

    Create hvw_xTabKontaktyDeleteLog.sql

---
+++ hvw_xTabKontaktyDeleteLog.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_xTabKontaktyDeleteLog]    Script Date: 04.07.2025 9:31:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_xTabKontaktyDeleteLog] AS select * from xTabKontaktyDeleteLog
+GO
+
