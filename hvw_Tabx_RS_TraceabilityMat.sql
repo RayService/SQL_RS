1e85e41011cee09cef2705130bb0e9411f28565a
RayService <michal.zufan@rayservice.com>
2025-07-04 08:53:47 +0200

    Create hvw_Tabx_RS_TraceabilityMat.sql

---
+++ hvw_Tabx_RS_TraceabilityMat.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_TraceabilityMat]    Script Date: 04.07.2025 8:53:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_TraceabilityMat] AS SELECT*FROM Tabx_RS_TraceabilityMat
+GO
+
