dc78f80dcf076331802e87f3f94f76976d5da7a2
RayService <michal.zufan@rayservice.com>
2025-07-04 08:41:29 +0200

    Create hvw_Tabx_RS_CNRuskeSankce3.sql

---
+++ hvw_Tabx_RS_CNRuskeSankce3.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_CNRuskeSankce3]    Script Date: 04.07.2025 8:41:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_CNRuskeSankce3] AS SELECT*FROM Tabx_RS_CNRuskeSankce3
+GO
+
