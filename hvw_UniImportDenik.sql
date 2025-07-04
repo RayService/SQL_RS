e89a5ebf1589bdae9b76aea684a3fc709916b85d
RayService <michal.zufan@rayservice.com>
2025-07-04 08:57:00 +0200

    Create hvw_UniImportDenik.sql

---
+++ hvw_UniImportDenik.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportDenik]    Script Date: 04.07.2025 8:56:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportDenik] AS SELECT *FROM TabUniImportDenik
+GO
+
