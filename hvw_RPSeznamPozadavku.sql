6d86bdfb143943cdb3dda65b2a1340b2f0260f5d
RayService <michal.zufan@rayservice.com>
2025-07-04 08:06:32 +0200

    Create hvw_RPSeznamPozadavku.sql

---
+++ hvw_RPSeznamPozadavku.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RPSeznamPozadavku]    Script Date: 04.07.2025 8:06:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RPSeznamPozadavku] AS SELECT * FROM GTabRPSeznamPozadavku
+GO
+
