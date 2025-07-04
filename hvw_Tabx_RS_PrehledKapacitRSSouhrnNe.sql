00be7b80d3e181d06a0607c063449c0fc5ed29de
RayService <michal.zufan@rayservice.com>
2025-07-04 08:49:17 +0200

    Create hvw_Tabx_RS_PrehledKapacitRSSouhrnNe.sql

---
+++ hvw_Tabx_RS_PrehledKapacitRSSouhrnNe.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_PrehledKapacitRSSouhrnNe]    Script Date: 04.07.2025 8:49:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_PrehledKapacitRSSouhrnNe] AS SELECT*FROM Tabx_RS_PrehledKapacitRSSouhrnNextYear
+GO
+
