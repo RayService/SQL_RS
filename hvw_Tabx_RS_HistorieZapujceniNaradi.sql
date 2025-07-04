9f8cf156923edc00dc0615854835de5c2c548d12
RayService <michal.zufan@rayservice.com>
2025-07-04 08:42:31 +0200

    Create hvw_Tabx_RS_HistorieZapujceniNaradi.sql

---
+++ hvw_Tabx_RS_HistorieZapujceniNaradi.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_HistorieZapujceniNaradi]    Script Date: 04.07.2025 8:42:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_HistorieZapujceniNaradi] AS SELECT*FROM Tabx_RS_HistorieZapujceniNaradi
+GO
+
