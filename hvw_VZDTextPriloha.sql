dfb890de533d6888f0bff872f9d293452fea998e
RayService <michal.zufan@rayservice.com>
2025-07-04 09:29:05 +0200

    Create hvw_VZDTextPriloha.sql

---
+++ hvw_VZDTextPriloha.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VZDTextPriloha]    Script Date: 04.07.2025 9:29:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VZDTextPriloha] AS SELECT * FROM Tabx_VZDTextPriloha
+GO
+
