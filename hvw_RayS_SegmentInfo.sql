8a3bce03dff1b88ac786d967a45176fc40ebe5a3
RayService <michal.zufan@rayservice.com>
2025-07-04 07:19:55 +0200

    Create hvw_RayS_SegmentInfo.sql

---
+++ hvw_RayS_SegmentInfo.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RayS_SegmentInfo]    Script Date: 04.07.2025 7:19:51 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RayS_SegmentInfo] AS SELECT * FROM RayS_SegmentInfo
+GO
+
