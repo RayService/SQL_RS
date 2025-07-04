ca2a84233f1177990caefafbd0a5e85e107a1750
RayService <michal.zufan@rayservice.com>
2025-07-04 08:05:35 +0200

    Create hvw_RPKonfigurace.sql

---
+++ hvw_RPKonfigurace.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RPKonfigurace]    Script Date: 04.07.2025 8:05:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RPKonfigurace] AS SELECT *  FROM GTabRPKonfigurace
+GO
+
