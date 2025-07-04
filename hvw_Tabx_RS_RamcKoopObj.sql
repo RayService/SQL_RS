e2143d67d7e63de11525d45ac66d82f6d5e177ec
RayService <michal.zufan@rayservice.com>
2025-07-04 08:51:36 +0200

    Create hvw_Tabx_RS_RamcKoopObj.sql

---
+++ hvw_Tabx_RS_RamcKoopObj.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_Tabx_RS_RamcKoopObj]    Script Date: 04.07.2025 8:50:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_Tabx_RS_RamcKoopObj] AS SELECT*FROM Tabx_RS_RamcKoopObj
+GO
+
