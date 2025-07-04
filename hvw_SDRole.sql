eb9e8251e095fbb1a0360a90275917ed7d98a6b4
RayService <michal.zufan@rayservice.com>
2025-07-04 08:20:49 +0200

    Create hvw_SDRole.sql

---
+++ hvw_SDRole.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_SDRole]    Script Date: 04.07.2025 8:20:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_SDRole] AS SELECT * FROM Tabx_SDRole
+GO
+
