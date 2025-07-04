3a2d28f69ea9c5c5370d97d966a99160f7886664
RayService <michal.zufan@rayservice.com>
2025-07-04 08:27:21 +0200

    Create hvw_SHTools_DeadlockDogSber.sql

---
+++ hvw_SHTools_DeadlockDogSber.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_SHTools_DeadlockDogSber]    Script Date: 04.07.2025 8:27:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_SHTools_DeadlockDogSber] AS SELECT*FROM Tabx_SHTools_DeadlockDogSber
+GO
+
