bd20542ea41697a03cd7459e25ab086da348ce15
RayService <michal.zufan@rayservice.com>
2025-07-04 07:07:50 +0200

    Create hvw_RayS_KlasifikaceVyrobku.sql

---
+++ hvw_RayS_KlasifikaceVyrobku.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RayS_KlasifikaceVyrobku]    Script Date: 04.07.2025 7:07:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RayS_KlasifikaceVyrobku] AS SELECT * FROM RayS_KlasifikaceVyrobku
+GO
+
