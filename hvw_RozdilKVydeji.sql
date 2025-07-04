1933406d7459bacddb9fc2645cafb520dbc3efaf
RayService <michal.zufan@rayservice.com>
2025-07-04 08:04:43 +0200

    Create hvw_RozdilKVydeji.sql

---
+++ hvw_RozdilKVydeji.sql
@@ -0,0 +1,29 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RozdilKVydeji]    Script Date: 04.07.2025 8:04:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RozdilKVydeji] AS SELECT SU.ID, SU.IDStavSkladu, SU.IDVyrCis, SU.IDUmisteni, KZ.SkupZbo, KZ.RegCis, U.Kod, SU.Mnozstvi, KZID=KZ.ID, SS.IDSklad
+FROM TabStavSkladu SS
+INNER JOIN TabKmenZbozi KZ ON KZ.ID=SS.IDKmenZbozi
+LEFT OUTER JOIN Gatema_StavUmisteni SU ON SU.IDStavSkladu=SS.ID
+LEFT OUTER JOIN TabUmisteni U ON U.Id=SU.IDUmisteni
+--INNER JOIN Tab_SDMater_Konfig K ON K.IDUmisteniRozdil=SU.IDUmisteni
+WHERE SU.Mnozstvi>0 AND U.Kod in (Select Um.kod From TabUmisteni Um Inner join Tab_SDMater_Konfig SDK ON SDK.IDUmisteniRozdil=Um.ID /*and SS.IDSklad=Um.IdSklad*/)
+
+/*
+SELECT SU.ID, SU.IDStavSkladu, SU.IDVyrCis, SU.IDUmisteni, KZ.SkupZbo, KZ.RegCis, U.Kod, SU.Mnozstvi, KZID=KZ.ID, SS.IDSklad
+FROM TabStavSkladu SS
+INNER JOIN TabKmenZbozi KZ ON KZ.ID=SS.IDKmenZbozi
+LEFT OUTER JOIN Gatema_StavUmisteni SU ON SU.IDStavSkladu=SS.ID
+LEFT OUTER JOIN TabUmisteni U ON U.Id=SU.IDUmisteni
+INNER JOIN Tab_SDMater_Konfig K ON K.IDUmisteniRozdil=SU.IDUmisteni
+WHERE SU.Mnozstvi>0
+*/
+GO
+
