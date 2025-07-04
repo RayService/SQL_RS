adb2394a2a7dde9cff4438e29073bdfb737ddc8e
RayService <michal.zufan@rayservice.com>
2025-07-04 09:28:05 +0200

    Create hvw_VZDSUCizinci.sql

---
+++ hvw_VZDSUCizinci.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VZDSUCizinci]    Script Date: 04.07.2025 9:28:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VZDSUCizinci] AS SELECT * FROM Tabx_VZDSUCizinci
+GO
+
