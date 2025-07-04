4dd74a224c98f4ed6110d1d95e995ccccf227a8c
RayService <michal.zufan@rayservice.com>
2025-07-04 08:05:58 +0200

    Create hvw_RPMailProfil.sql

---
+++ hvw_RPMailProfil.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RPMailProfil]    Script Date: 04.07.2025 8:05:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RPMailProfil] AS SELECT * FROM GTabRPMailProfil
+GO
+
