eff44440bb97cc38d86f242eb7035e5eb6688a17
RayService <michal.zufan@rayservice.com>
2025-07-04 09:15:49 +0200

    Create hvw_UniImportZam.sql

---
+++ hvw_UniImportZam.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportZam]    Script Date: 04.07.2025 9:14:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportZam] AS SELECT *FROM TabUniImportZam
+GO
+
