0fd7e8b7e8020cc987bc26cc476935685254fe4b
RayService <michal.zufan@rayservice.com>
2025-07-04 09:18:49 +0200

    Create hvw_VSDVyuct.sql

---
+++ hvw_VSDVyuct.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_VSDVyuct]    Script Date: 04.07.2025 9:18:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_VSDVyuct] AS SELECT * FROM Tabx_VSDVyuct
+GO
+
