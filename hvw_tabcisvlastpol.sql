7abded00ded1a7c9ec2eabd42adaa511d677685a
RayService <michal.zufan@rayservice.com>
2025-07-04 08:39:11 +0200

    Create hvw_tabcisvlastpol.sql

---
+++ hvw_tabcisvlastpol.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_tabcisvlastpol]    Script Date: 04.07.2025 8:39:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_tabcisvlastpol] AS select * from e_properties
+GO
+
