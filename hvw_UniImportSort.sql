5fa3ef982cfa10aa4daed1c65238ea455617e12d
RayService <michal.zufan@rayservice.com>
2025-07-04 09:12:07 +0200

    Create hvw_UniImportSort.sql

---
+++ hvw_UniImportSort.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_UniImportSort]    Script Date: 04.07.2025 9:11:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_UniImportSort] AS SELECT *FROM TabUniImportSort
+GO
+
