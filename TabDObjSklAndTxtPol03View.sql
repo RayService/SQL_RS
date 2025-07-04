72059bd3f81bc3c07a8adad03735b0e91a4c8e87
RayService <michal.zufan@rayservice.com>
2025-07-04 09:52:20 +0200

    Create TabDObjSklAndTxtPol03View.sql

---
+++ TabDObjSklAndTxtPol03View.sql
@@ -0,0 +1,21 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[TabDObjSklAndTxtPol03View]    Script Date: 04.07.2025 9:52:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[TabDObjSklAndTxtPol03View] AS
+SELECT IDHlava, ID AS IDPolozky, NULL AS IDTxtPol, ID AS IDUni, PoradiPolozky AS Poradi,
+CAST(SkupZbo+' '+RegCis+' '+Nazev1 AS NVARCHAR(200)) AS Popis
+FROM TabDosleObjR03
+UNION ALL
+SELECT IDHlava, NULL, ID, -ID, Poradi,
+CAST(CAST(Poradi AS NVARCHAR (20))+', '+
+CAST(Popis  AS NVARCHAR(200)) AS NVARCHAR(200))
+FROM TabDosleObjTxtPol03
+GO
+
