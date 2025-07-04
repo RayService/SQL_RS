9870e7aef269fb3369a97dc0d9f34730b316e0df
RayService <michal.zufan@rayservice.com>
2025-07-04 07:19:33 +0200

    Create hvw_RayS_MarketingInfo.sql

---
+++ hvw_RayS_MarketingInfo.sql
@@ -0,0 +1,13 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[hvw_RayS_MarketingInfo]    Script Date: 04.07.2025 7:19:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[hvw_RayS_MarketingInfo] AS SELECT * FROM RayS_MarketingInfo
+GO
+
