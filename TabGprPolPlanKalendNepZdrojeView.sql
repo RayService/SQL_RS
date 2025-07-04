81b5fb8eeed0b9c2da655235ec451f724b3204bf
RayService <michal.zufan@rayservice.com>
2025-07-04 11:08:33 +0200

    Create TabGprPolPlanKalendNepZdrojeView.sql

---
+++ TabGprPolPlanKalendNepZdrojeView.sql
@@ -0,0 +1,24 @@
+USE [RayService]
+GO
+
+/****** Object:  View [dbo].[TabGprPolPlanKalendNepZdrojeView]    Script Date: 04.07.2025 11:08:28 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[TabGprPolPlanKalendNepZdrojeView] AS 
+SELECT 
+ IDGprNepZdroje=NZ.ID, 
+ IDPlanKalend=PKP.IDPlanKalend, 
+ IDPlanKalendPol=PKP.ID, 
+ DatumPripadu=CASE WHEN S.TypSmeny=1 AND PKP.MinDo<=720 THEN PKP.datum-1 
+                   WHEN S.TypSmeny=2 AND PKP.MinOd>=720 THEN PKP.datum+1 
+                   ELSE PKP.datum END, 
+ PKP.datum, PKP.MinOd, PKP.MinDo, PKP.KoefPlneni, PKP.IDSmeny, PKP.CasOd, PKP.CasDo 
+   FROM TabGprNepZdroje NZ
+     INNER JOIN TabPlanKalendPol PKP ON (PKP.minOd IS NOT NULL AND PKP.IDPlanKalend = NZ.IDPlanKalend) 
+     LEFT OUTER JOIN TabCSmeny S ON (S.ID=PKP.IDSmeny)
+GO
+
